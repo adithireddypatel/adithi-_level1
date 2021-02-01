0c2cc2253f256c3f45f385bf3378fbd8a64d7e75
@adithireddyp <70527687+adithireddypatel@users.noreply.github.com>
2021-02-01 19:24:52 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -5,7 +5,7 @@ void main()
    float h,d,b;
    float volume_of_tromboloid;
    printf("enter the values of h,d,b:");
-   scanf("%f%f%f",&h,&d,&,b);
+   scanf("%f%f%f",&h,&d,&b);
    volume_of_tromboloid = (((h*d*b)+(d/b))/3);
-   printf('volume of tromboloid is %f",volume_of_tromboloid);
+   printf("volume of tromboloid is %f",volume_of_tromboloid);
 }
